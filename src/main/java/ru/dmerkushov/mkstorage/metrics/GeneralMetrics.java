a457b6626d4ce24b5f12eb8503a5547c4684e551
Dmitriy Merkushov <d.merkushov@gmail.com>
2022-01-08 21:18:47 +0300

    += item remove ability (HTTP DELETE method)
    Improve GeneralMetrics

--- src/main/java/ru/dmerkushov/mkstorage/metrics/GeneralMetrics.java
+++ src/main/java/ru/dmerkushov/mkstorage/metrics/GeneralMetrics.java
@@ -2,49 +2,74 @@ package ru.dmerkushov.mkstorage.metrics;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import lombok.RequiredArgsConstructor;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class GeneralMetrics {
 
     private final MeterRegistry meterRegistry;
 
+    public static final String NAME_ERROR_MULTIPLE_FOUND_COUNTER = "storage_error_multiple_found_counter";
+    public static final String NAME_ERROR_NOT_FOUND_COUNTER = "storage_error_not_found_counter";
+
+    public static final String OPER_GET = "get";
+    public static final String OPER_STORE = "store";
+    public static final String OPER_REMOVE = "remove";
+
+    public final Map<String, String> counterDescriptions;
+
+    public GeneralMetrics(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+
+        Map<String, String> counterDescriptionsDraft = new HashMap<>();
+        counterDescriptionsDraft.put(NAME_ERROR_MULTIPLE_FOUND_COUNTER, "Cases of finding multiple values for the given tag list");
+        counterDescriptionsDraft.put(NAME_ERROR_NOT_FOUND_COUNTER, "Cases of not finding any values for the given tag list");
+
+        this.counterDescriptions = Collections.unmodifiableMap(counterDescriptionsDraft);
+    }
+
     @Bean
     public Counter errorMultipleFoundCounterGet() {
-        return Counter
-                .builder("storage_error_multiple_found_counter")
-                .tag("method", "get")
-                .description("Counter for cases of finding multiple values for the given tag list")
-                .register(meterRegistry);
+        return buildCounter(NAME_ERROR_MULTIPLE_FOUND_COUNTER, OPER_GET);
     }
 
     @Bean
     public Counter errorNotFoundCounterGet() {
-        return Counter
-                .builder("storage_error_not_found_counter")
-                .tag("method", "get")
-                .description("Counter for cases of not finding any values for the given tag list")
-                .register(meterRegistry);
+        return buildCounter(NAME_ERROR_NOT_FOUND_COUNTER, OPER_GET);
     }
 
     @Bean
     public Counter errorMultipleFoundCounterStore() {
-        return Counter
-                .builder("storage_error_multiple_found_counter")
-                .tag("method", "store")
-                .description("Counter for cases of finding multiple values for the given tag list")
-                .register(meterRegistry);
+        return buildCounter(NAME_ERROR_MULTIPLE_FOUND_COUNTER, OPER_STORE);
     }
 
     @Bean
     public Counter errorNotFoundCounterStore() {
+        return buildCounter(NAME_ERROR_NOT_FOUND_COUNTER, OPER_STORE);
+    }
+
+    @Bean
+    public Counter errorMultipleFoundCounterRemove() {
+        return buildCounter(NAME_ERROR_MULTIPLE_FOUND_COUNTER, OPER_REMOVE);
+    }
+
+    @Bean
+    public Counter errorNotFoundCounterRemove() {
+        return buildCounter(NAME_ERROR_NOT_FOUND_COUNTER, OPER_REMOVE);
+    }
+
+    private Counter buildCounter(
+            String name,
+            String operationTag
+    ) {
         return Counter
-                .builder("storage_error_not_found_counter")
-                .tag("method", "store")
-                .description("Counter for cases of not finding any values for the given tag list")
+                .builder(name)
+                .tag("operation", operationTag)
+                .description(counterDescriptions.get(name))
                 .register(meterRegistry);
     }
 }
